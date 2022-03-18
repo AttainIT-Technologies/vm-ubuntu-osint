598d00995ab771beabef9df8813f4d8738337ab1
cmhettinger <cmhettinger@hotmail.com>
2022-03-18 17:28:01 -0400

    user.js

--- ff-user.js
+++ ff-user.js
@@ -1,8 +1,24 @@
 
 
-# about:config
+// about:config
 
 // Disable Geolocation
 // https://www.mozilla.org/en-US/firefox/geolocation/
 user_pref("geo.enabled", false);
 
+// Disable WebRTC and related settings
+// https://wiki.mozilla.org/Media/WebRTC/Privacy
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.peerconnection.ice.no_host",  true);
+user_pref("media.peerconnection.turn.disable", true);
+user_pref("media.peerconnection.use_document_iceservers", false);
+user_pref("media.peerconnection.video.enabled", false);
+user_pref("media.peerconnection.video.enabled", false);
+
+// Disable Media Navigator
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+user_pref("media.navigator.enabled", false);
+
+// Disable Battery API
+// https://developer.mozilla.org/en-US/docs/Web/API/BatteryManager
+user_pref("dom.battery.enabled", false);
