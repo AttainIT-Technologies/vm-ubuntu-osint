8f00984167839e1bf5736325edfa85cb121fcd33
cmhettinger <cmhettinger@hotmail.com>
2022-03-20 09:10:33 -0400

    user.js

--- ff-user.js
+++ ff-user.js
@@ -2,6 +2,27 @@
 
 // about:config
 
+// Settings | General
+// Disable "Recommend extensions/features as you browse"
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+
+// Settings | Home
+// Home Page and New Windows = blank page
+// New Tabs = blank page
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.url", "about:blank")
+user_pref("browser.startup.homepage", "about:blank")
+
+// Settings | Privacy & Security
+// Set Enhanced Tracking Protection = Strict
+user_pref("browser.contentblocking.category", "strict");
+user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
+user_pref("privacy.annotate_channels.strict_list.enabled", true);
+user_pref("privacy.partition.network_state.ocsp_cache", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+
 // Disable Geolocation
 // https://www.mozilla.org/en-US/firefox/geolocation/
 user_pref("geo.enabled", false);
